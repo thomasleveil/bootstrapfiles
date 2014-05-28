5580f177d82db3fbb6e0e8815b6a55fb722b48b5
Thomas LEVEIL <thomas.leveil@alyotech.fr>
2014-05-28 11:04:47 +0200

    fix docker.sh

--- ubuntu/trusty/docker.sh
+++ ubuntu/trusty/docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 set -u
-YELLOW="\033[COLORm"
+YELLOW="\033[33m"
 RS="\033[0m"
 
 test "$(whoami)" = "root"
@@ -12,7 +12,7 @@ apt-get install -y docker.io
 ## alias docker.io to docker
 ln -sf /usr/bin/docker.io /usr/local/bin/docker
 if [[ $(egrep -c '^complete -F _docker docker$' /etc/bash_completion.d/docker.io) -eq 0 ]]; then
-	echo "complete -F _docker docker" >> /etc/bash_completion.d/docker.io
+        echo "complete -F _docker docker" >> /etc/bash_completion.d/docker.io
 fi
 
-echo -e "${YELLOW}Add users to the 'docker'${RS}"
+echo -e "${YELLOW}Add users to the 'docker' group${RS}"
