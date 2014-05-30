ea95b909803375a24eb64e52fa014bb9d3d0bac7
Thomas LÉVEIL <thomasleveil@users.noreply.github.com>
2014-05-30 02:28:46 +0200

    Update docker.sh
    to install the latest docker instead of outdated docker from the Ubunutu repo

--- ubuntu/trusty/docker.sh
+++ ubuntu/trusty/docker.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 set -e
 set -u
+set -o pipefail
 YELLOW="\033[33m"
 RS="\033[0m"
 
+function my_trap_handler()
+{
+        MYSELF="$0"               # equals to my script name
+        LASTLINE="$1"            # argument 1: last line of error occurence
+        LASTERR="$2"             # argument 2: error code of last command
+        echo "${MYSELF}: line ${LASTLINE}: exit status of last command: ${LASTERR}"
+}
+trap 'my_trap_handler ${LINENO} ${$?}' ERR
+######################################################################################
+
 test "$(whoami)" = "root"
 
-## install docker
-apt-get install -y docker.io
 
-## alias docker.io to docker
-ln -sf /usr/bin/docker.io /usr/local/bin/docker
-if [[ $(egrep -c '^complete -F _docker docker$' /etc/bash_completion.d/docker.io) -eq 0 ]]; then
-        echo "complete -F _docker docker" >> /etc/bash_completion.d/docker.io
-fi
+## install docker
+curl -sL https://get.docker.io/ | sh
+curl -sL https://raw.githubusercontent.com/dotcloud/docker/master/contrib/completion/bash/docker > /etc/bash_completion.d/docker
+
 
 echo -e "${YELLOW}Add users to the 'docker' group${RS}"
